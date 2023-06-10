94963f3884d50acfb5794cd3ae88f109d6915561
Divy <divyparekh1810@gmail.com>
2023-06-10 11:36:55 +0530

    created signUp and signIn routes

--- index.js
+++ index.js
@@ -4,46 +4,85 @@ const port = 3001
 
 const USERS = [];
 
-const QUESTIONS = [{
-    title: "Two states",
-    description: "Given an array , return the maximum of the array?",
-    testCases: [{
-        input: "[1,2,3,4,5]",
-        output: "5"
-    }]
-}];
+const QUESTIONS = [
+    {
+        title: "Maximum Number",
+        description: "Given an array , return the maximum of the array?",
+        testCases: [
+            {
+                input: "[1,2,3,4,5]",
+                output: "5"
+            },
+            {
+                input: "[10,20,30,40,50]",
+                output: "50"
+            }
+        ]
+    },
+    {
+        title: "Minimum Number",
+        description: "Given an array , return the minimum of the array?",
+        testCases: [
+            {
+                input: "[1,2,3,4,5]",
+                output: "1"
+            },
+            {
+                input: "[10,20,30,40,50]",
+                output: "10"
+            }
+        ]
+    },
+];
 
 
 const SUBMISSION = [
 
 ]
 
+// Middleware to parse the request body
+app.use(express.json());
+
+app.get('/', function (req, res) {
+    res.send("Hello, Welcome to the LeetCode Clone")
+})
+
 app.post('/signup', function (req, res) {
     // Add logic to decode body
-    // body should have email and password
+    const { email, password } = req.body;
 
+    //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesn't exist)
 
-    //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
+    const userExists = USERS.some(user => user.email === email);
+    if (userExists) {
+        return res.status(400).json({ error: 'User with this email already exists' });
+    }
 
+    const newUser = { email, password };
+    USERS.push(newUser);
 
     // return back 200 status code to the client
-    res.send('Hello World!')
+    res.status(200).json({ message: 'Signup successful' });
 })
 
 app.post('/login', function (req, res) {
-    // Add logic to decode body
-    // body should have email and password
+    // Decode the request body
+    const { email, password } = req.body;
 
-    // Check if the user with the given email exists in the USERS array
-    // Also ensure that the password is the same
+    // Find the user with the given email in the USERS array
+    const user = USERS.find(user => user.email === email);
 
+    // Check if the user exists and the password matches
+    if (user && user.password === password) {
+        // Generate a random token (for demonstration purposes only)
+        const token = Math.random().toString(36).substring(7);
 
-    // If the password is the same, return back 200 status code to the client
-    // Also send back a token (any random string will do for now)
-    // If the password is not the same, return back 401 status code to the client
-
-
-    res.send('Hello World from route 2!')
+        // Return a success response with the token
+        res.status(200).json({ message: 'Login successful', token });
+    } else {
+        // Return an unauthorized response
+        res.status(401).json({ error: 'Invalid email or password' });
+    }
 })
 
 app.get('/questions', function (req, res) {
@@ -63,6 +102,11 @@ app.post("/submissions", function (req, res) {
     // Store the submission in the SUBMISSION array above
     res.send("Hello World from route 4!")
 });
+
+app.get('/users', function (req, res) {
+    res.status(200).json(USERS);
+});
+
 
 // leaving as hard todos
 // Create a route that lets an admin add a new problem
